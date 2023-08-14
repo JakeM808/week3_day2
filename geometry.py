77654eb24bee73abf156326ad52482b003992e67
Jake Matthews <jake.m86@gmail.com>
2023-08-14 05:04:16 -1000

    first_commit

---
+++ geometry.py
@@ -0,0 +1,7 @@
+from math import pi, sqrt
+
+def circle_area(radius):
+    return pi * radius**2
+
+def get_hypotenuse(a, b):
+    return sqrt(a**2 + b**2)
---
+++ whiteboard.py
@@ -0,0 +1,23 @@
+# Given a word w 
+# and some text t, find whether w is in t. 
+# For example: 
+#w="nest", t="I built a nest and tested it."
+#output: "here"
+#example 2:
+#w="runs", t="The dog"
+#output: "not here"
+#Example 3: 
+#w="April", t="april showers bring may flowers"
+#output: "not here"
+
+
+# eval the word (w) and see if its in text (t)
+
+def solution(w, t):
+    for word in t.split():
+        if word ==w
+        return "here"
+    else:
+        return "not here"
+    
+    
